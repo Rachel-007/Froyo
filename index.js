dd1690656e3f5fea078e32329c94ad6599d970d8
Rachel Davis <rachelddavis@aol.com>
2025-04-24 19:38:37 -0600

    Froyo js updates

--- index.js
+++ index.js
@@ -9,9 +9,19 @@ const User = {"FirstName": "John", "LastName": "Doe",}
 
 console.log("Welcome to Froyo," + User.FirstName + " " + User.LastName + "!");
 console.log("Your Order:");
-console.log(flavors.vanilla + " scoops of vanilla, " + flavors.strawberry + " scoops of strawberry, " + flavors.coffee + " scoops of coffee, " + flavors.mint + " scoops of mint.");
+
+
+
+
+for (const flavor in flavors) {
+    console.log(`${flavors[flavor]} scoops of ${flavor}`);
+}
+
+const totalScoops = Object.values(flavors).reduce((total, num) => total + parseInt(num), 0);
+
+console.log(`Total scoops ordered by ${User.FirstName} ${User.LastName}: ${totalScoops}`);
+
+
 
 
 console.log("Enjoy your froyo!");
-
-
